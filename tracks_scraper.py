56a6eafda97e27f063069c58d184624765e8193d
Phillip Coletti <phillcoletti@Phillips-MacBook-Pro.local>
2017-11-29 21:28:10 -0800

    Full tracks script written with logger. Not yet tested

--- tracks_scraper.py
+++ tracks_scraper.py
@@ -1,26 +1,12 @@
 #! /usr/bin/env python
-from __future__ import unicode_literals
 
-import argparse
-import demjson
 import os
-import re
 import requests
 import soundcloud
-import sys
-import urllib
 import pandas as pd
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-#from clint.textui import colored, puts, progress
 from datetime import datetime
-#from mutagen.mp3 import MP3, EasyMP3
-#from mutagen.id3 import APIC, WXXX
-#from mutagen.id3 import ID3 as OldID3
-from subprocess import Popen, PIPE
-from os.path import dirname, exists, join
-from os import access, mkdir, W_OK
+from time import sleep
+import logging
 
 ####################################################################
 
@@ -32,11 +18,31 @@ MAGIC_CLIENT_ID = 'b45b1aa10f1ac2941910a7f0d10f8e28'
 AGGRESSIVE_CLIENT_ID = 'OmTFHKYSMLFqnu2HHucmclAptedxWXkq'
 APP_VERSION = '1481046241'
 
+# page size of 200 seems to work nicely, even though the API
+# seems to return 180-200 results per request
+PAGE_SIZE = 200
+# Sleep time between requests - 2 seconds so as to not overload the server
+SLEEP_TIME = 2
+
 ####################################################################
 
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./footwork_tracks.log',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
-# In[2]:
-
+####################################################################
 
 def get_client():
     """
@@ -45,73 +51,47 @@ def get_client():
     client = soundcloud.Client(client_id=CLIENT_ID)
     return client
 
-
-# In[3]:
-
-
-# get client
+### Main Script ###
 client = get_client()
 
-
-# In[38]:
-
-
-# find all sounds of footwork'
-page_size = 200
-response = client.get('/tracks', q='footwork', limit=page_size,
+# find all tracks with footwork tag
+response = client.get('/tracks', q='footwork', limit=PAGE_SIZE,
                     linked_partitioning=1)
-#for tracks in tracks.collection:
-#    print(track.title)
-
-
-# In[39]:
-
-
+counter = 1
+# parse dictionary of response
 response_dict = response.fields()
-next_href = response_dict['next_href']
-results = response_dict['collection']
-result_keys = results[0].keys()
-data = pd.DataFrame(results)
-data = data.drop(columns=['description'])
+next_href = False
+if('next_href' in response_dict):
+	next_href = response_dict['next_href']
+if('collection' in response_dict):
+	results = response_dict['collection']
+	result_keys = results[0].keys()
+	data = pd.DataFrame(results)
+	data = data.drop(columns=['description'])
+	# write initial dataframe to file, overwriting any previous file
+	data.to_csv('footwork_tracks.csv')
+while (next_href):
+	counter = counter + 1
+	response_dict = requests.get(next_href).json()
+	if('next_href' in response_dict):
+		next_href = response_dict['next_href']
+	else:
+		next_href = False
+	if('collection' in response_dict):
+		results = response_dict['collection']
+		result_keys = results[0].keys()
+		data = pd.DataFrame(results)
+		data = data.drop(columns=['description'])
+		# append next set of results to file
+		data.to_csv('footwork_tracks.csv', mode='a', header=False)
+		logging.info('Got ' + str(len(data))) + ' results from request ' + str(counter)
+	else:
+		# Now, we can log to the root logger, or any other logger. First the root...
+		logging.info('No collection of results returned')
+		#print("No collection of results returned")
+	logging.info('Sleeping ' + str(SLEEP_TIME) + ' seconds at ' + str(datetime.now()))
+	#print('Sleeping ' + SLEEP_TIME + ' seconds at ' + str(datetime.now()))
+	sleep(SLEEP_TIME)
 
 
-# In[40]:
-
-
-data['title'][len(data)-1]
-
-
-# In[41]:
-
-
-data.to_csv('footwork_tracks.csv')
-
-
-# In[42]:
-
-
-response_dict = requests.get(next_href).json()
-next_href = response_dict['next_href']
-results = response_dict['collection']
-result_keys = results[0].keys()
-new_data = pd.DataFrame(results)
-new_data = new_data.drop(columns=['description'])
-
-
-# In[43]:
-
-
-new_data
-
-
-# In[44]:
-
-
-new_data['title'][0]
-
-
-# In[45]:
-
-
-new_data.to_csv('footwork_tracks.csv', mode='a', header=False)
 
