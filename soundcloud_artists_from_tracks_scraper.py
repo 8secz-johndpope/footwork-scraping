6dc4f3d856279cb9a51be067bcdb031c983a2ac9
Phillip Coletti <phillcoletti@Phillips-MacBook-Pro.local>
2017-12-06 21:00:45 -0800

    Wrote initial script for pulling artists from tracks

--- soundcloud_artists_from_tracks_scraper.py
+++ soundcloud_artists_from_tracks_scraper.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[37]:
-
-
 #! /usr/bin/env python
 
 import os
@@ -28,16 +22,30 @@ APP_VERSION = '1481046241'
 
 ####################################################################
 
-# Sleep time between requests - 2 seconds so as to not overload the server
-SLEEP_TIME = 2
+# Sleep time between requests - 4 seconds so as to not overload the server
+SLEEP_TIME = 4
 inFileName = './data/soundcloud_footwork_tracks_tag.csv'
 outFileName = './data/soundcloud_footwork_artists_from_tracks.csv'
 
 ####################################################################
 
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./logs/footwork_artists_from_tracks.log',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
-# In[5]:
-
+####################################################################
 
 def get_client():
     """
@@ -46,127 +54,48 @@ def get_client():
     client = soundcloud.Client(client_id=CLIENT_ID)
     return client
 
+def get_user_dict_from_client(client, user_id):
+	query = '/users/' + str(user_id)
+	response = client.get(query)
+	user = response.fields()
+	user['description'] = user['description'].replace('\n', ' ')
+			.encode("ascii", errors="ignore").decode().strip()
+	return user
 
-# In[6]:
-
-
-# get client
+### Main Script ###
 client = get_client()
-
-
-# In[5]:
-
-
-# find all sounds of footwork'
-response = client.get('/users', q='footwork', limit=page_size,
-                    linked_partitioning=1)
-#for tracks in tracks.collection:
-#    print(track.title)
-
-
-# In[12]:
-
-
-response_dict = response.fields()
-next_href = response_dict['next_href']
-results = response_dict['collection']
-result_keys = results[0].keys()
-data = pd.DataFrame(results)
-data = data.drop(columns=['description'])
-
-
-# In[40]:
-
-
-data['title'][len(data)-1]
-
-
-# In[13]:
-
-
-data.to_csv('footwork_artists.csv')
-
-
-# In[42]:
-
-
-response_dict = requests.get(next_href).json()
-next_href = response_dict['next_href']
-results = response_dict['collection']
-result_keys = results[0].keys()
-new_data = pd.DataFrame(results)
-new_data = new_data.drop(columns=['description'])
-
-
-# In[45]:
-
-
-new_data.to_csv('footwork_tracks.csv', mode='a', header=False)
-
-
-# In[7]:
-
 
 # Generate list of artists to query from list
 # of tracks tagged with footwork
 tracks = pd.read_csv(inFileName)
-
-
-# In[14]:
-
-
-tracks.columns
-
-
-# In[97]:
-
-
-tracks['download_count'][0]
-
-
-# In[47]:
-
-
-response = client.get('/users/1918080')
-
-
-# In[48]:
-
-
-user = response.fields()
-
-
-# In[49]:
-
-
-user['description'] = user['description'].replace('\n', ' ').encode("ascii", errors="ignore").decode().strip()
-
-
-# In[50]:
-
-
-with open(outFileName, 'w') as outFile:  # Just use 'w' mode in 3.x
-    outFileWriter = csv.DictWriter(outFile, user.keys())
-    outFileWriter.writeheader()
-    outFileWriter.writerow(user)
-
-
-# In[ ]:
-
-
-# Group by artist to get aggregate stats so we can sort and query in a sensible order
+# Group and agg by artist so that we can sort and query in a sensible order
 # sum is the total number of downlaods per artist across all tracks in list
 # count is the total number of tracks in the list per artist
 grouped = tracks[['user_id', 'download_count']].groupby('user_id')
 aggregated = grouped.agg(['sum', 'count'])
 # remove the multiindex
 aggregated.columns = aggregated.columns.droplevel()
-sorted_artists = aggregated.sort_values(by='sum', ascending=False)
-sorted_user_id_list = list(sorted_artists.index)
-
-
-# In[104]:
-
-
-
+sorted_artist_data = aggregated.sort_values(by='sum', ascending=False)
+sorted_user_id_list = list(sorted_artist_data.index)
+with open(outFileName, 'w') as outFile:
+	try:
+		# run the first query and set up the csv file with a header row
+		user = get_user_dict_from_client(client, sorted_user_id_list.pop(0))
+	    outFileWriter = csv.DictWriter(outFile, user.keys())
+	    outFileWriter.writeheader()
+	    outFileWriter.writerow(user)
+	    logging.info('First Entry: user_id: ' + user['id'])
+	    # Interate through the entire list of artists
+	    for i, user_id in enumerate(sorted_user_id_list):
+	    	logging.info('Sending request # ' + str(i) + ' for user_id: ' 
+	    			+ str(user_id))
+	    	user = get_user_dict_from_client(client, user_id)
+	    	outFileWriter.writerow(user)
+	    	logging.info('Successfully recorded user in csv; ' + 
+	    		'Sleeping ' + str(SLEEP_TIME) + ' seconds at ' + str(datetime.now()))
+	    	sleep(SLEEP_TIME)
+	    logging.info('All artists pulled. Finishing at ' + str(datetime.now()))
+    
+	except Exception as e:
+		logging.info('Error from API request: ' + str(e))
 
